2b186feaffeeb68e0bdf86684047b9d60907999b
Zareix <raphcatarino@gmail.com>
2021-01-04 18:43:26 +0100

    init w/ other services' base

--- projet_client_local/src/application_client_locale/ApplicationClientLocale.java
+++ projet_client_local/src/application_client_locale/ApplicationClientLocale.java
@@ -1,7 +1,55 @@
 package application_client_locale;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class ApplicationClientLocale {
-	public static void main(String[] args) {
+	private final static String HOST = "localhost";
+	private final static int PORT_RESERVATION = 3000;
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket socket = new Socket(HOST, PORT_RESERVATION);
+
+		BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(socketIn.readLine());
+
+		// Entrer le numéro d'abonné
+		while (true) {
+			String s = socketIn.readLine();
+			System.out.println(s);
+
+			if (s.matches("([Bienvenue].*)"))
+				break;
+
+			socketOut.println(sc.nextLine());
+		}
+
+		// Affichage liste de docs
+		while (socketIn.ready()) {
+			System.out.println(socketIn.readLine());
+		}
+
+		// Réservation d'un livre
+		while (true) {
+			String s = sc.nextLine();
+			socketOut.println(s);
+
+			System.out.println(socketIn.readLine());
+			
+			if (s.equalsIgnoreCase("terminer"))
+				break;
+		}
 		
+		sc.close();
+		socket.close();
 	}
 }
--- projet_serveur/src/réservation/ServiceReservation.java
+++ projet_serveur/src/réservation/ServiceReservation.java
@@ -1,11 +1,16 @@
 package réservation;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
 import application_serveur.Abonne;
 import application_serveur.Documents;
+import exception.ReservationException;
 
 public class ServiceReservation implements Runnable {
 	static List<Abonne> abonnes = new ArrayList<>();
@@ -19,7 +24,81 @@ public class ServiceReservation implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO : service réservation
+		try {
+			BufferedReader socketIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			PrintWriter socketOut = new PrintWriter(client.getOutputStream(), true);
+
+			socketOut.println("Connexion au service de réservation.\nMerci de renseigner votre numéro de client");
+
+			Abonne ab = null;
+			// TODO : factoriser connexion
+			// Connexion
+			while (true) {
+				String s = socketIn.readLine();
+				int numAbo = -1;
+				if (!s.matches("-?\\d+")) {
+					socketOut.println("Merci d'entrer un numéro valide");
+				} else {
+					numAbo = (int) Integer.valueOf(s);
+					for (Abonne abonne : abonnes) {
+						if (abonne.getId() == numAbo) {
+							ab = abonne;
+							break;
+						}
+					}
+					if (ab == null)
+						socketOut.println("Ce numéro d'abonné n'est pas reconnu");
+					else
+						break;
+				}
+			}
+
+			socketOut.println("Bienvenue " + ab.getNom() + "\nVoici la liste des documents :");
+
+			// Affichage des documents
+			List<Documents> docAbo = ab.getDocuments();
+			for (Documents doc : docAbo) {
+				socketOut.println("  - " + doc);
+			}
+			socketOut.println(
+					"Veuillez saisir le numéro du document que vous souhaitez retourner\nTapez \"terminer\" pour mettre fin au service d'emprunt");
+			socketOut.println("finListe");
+
+			// Réservation d'un documents
+			while (true) {
+				String s = socketIn.readLine();
+				if (s.equalsIgnoreCase("terminer")) {
+					socketOut.println("Merci d'avoir utiliser le service de réservation");
+					try {
+						client.close();
+					} catch (IOException e2) {
+					}
+					;
+				}
+				int numDoc = -1;
+				boolean docFound = false;
+				if (s.matches("-?\\d+")) {
+					numDoc = (int) Integer.valueOf(s);
+					for (Documents doc : documents) {
+						if (doc.numero() == numDoc) {
+							docFound = true;
+							try {
+								doc.reservationPour(ab);
+								socketOut.println("Document réservé avec succès !");
+							} catch (ReservationException e) {
+								socketOut.println(e.getMessage());
+							}
+						}
+					}
+					if (!docFound)
+						socketOut.println("Ce numéro de document n'existe pas");
+				} else {
+					socketOut.println("Merci de rentrer un numéro valide");
+				}
+			}
+		} catch (IOException e) {
+			// Fin service retour
+		}
 	}
 
 	protected void finalize() throws Throwable {
