b93317ce29a5caaac5c8f98c9f66aeb3356ced91
Zareix <raphcatarino@gmail.com>
2021-01-08 15:21:58 +0100

    début factorisation

--- projet_serveur/src/application_serveur/ServiceTools.java
+++ projet_serveur/src/application_serveur/ServiceTools.java
@@ -6,19 +6,19 @@ import java.io.PrintWriter;
 import java.util.List;
 
 /**
- * Différents outils utilisés par les services d'emprunt, retour et réservation
- *
+ * Différents outils utilisés par les services d'emprunt, retour et réservation.
+ * <br>
+ * Permet de factoriser le code des services et d'éviter les redondances.
  */
 public class ServiceTools {
 	public static Abonne connexion(BufferedReader socketIn, PrintWriter socketOut, List<Abonne> abonnes)
 			throws IOException {
 		while (true) {
 			String s = socketIn.readLine();
-			int numAbo = -1;
 			if (!s.matches("-?\\d+")) {
 				socketOut.println("Merci d'entrer un numéro valide");
 			} else {
-				numAbo = (int) Integer.valueOf(s);
+				int numAbo = Integer.valueOf(s);
 				for (Abonne abonne : abonnes) {
 					if (abonne.getId() == numAbo)
 						return abonne;
@@ -31,9 +31,8 @@ public class ServiceTools {
 	public static void affichageDocs(PrintWriter socketOut, List<Document> documents) {
 		socketOut.println("Voici la liste des documents :");
 		String s = "";
-		for (Document doc : documents) {
-			s += "  - " + doc;
-		}
+		for (Document doc : documents)
+			s += "  - " + doc + "\n";
 		s += "finliste";
 		socketOut.println(s);
 	}
